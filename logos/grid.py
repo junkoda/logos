Get normalization of input velocity power spectrum correct.

logos/grid.py
@@ -66,7 +66,7 @@ class Grid:
         assert(self.mode == 'fourier-space')
         self.mode == 'real-space'
 
-        self.fx = np.fft.irfft(uk)/boxsize
+        self.fx = np.fft.irfft(uk)*(self.nc/boxsize)
         self.mode == 'real-space'
 
         if self.shifted is not None:

logos/init.py
@@ -1,7 +1,12 @@
+import math
+import numpy as np
 
 def velocity_field(nc, boxsize, *, sigma_u=10.0, sigmap=1.0):
-    """
-    Generate a velocity field with power spectrum
+    """Generate a velocity field with power spectrum
+
+    Args:
+        Pvel: velocity power spectrum
+        sigma_u (float): normalize Pvel(k) to this velocity rms
     Pvel(k) = A*exp[ -(k*sigmap)**2 ]
     <u(x)^2> = sigma_u
 
@@ -12,19 +17,23 @@ def velocity_field(nc, boxsize, *, sigma_u=10.0, sigmap=1.0):
     uk = np.zeros(nk, dtype=complex)
     dk = 2.0*math.pi/boxsize
 
-    k = dk*np.zeros(nc//2 + 1)
+    k = dk*np.arange(nk)
 
-    # amplitude of u(k) = sqrt(V*Pu(k))
-    ampfac = amp*sigma_u/(4*math.sqrt(math.pi))
-    amp = math.sqrt(ampfac*boxsize)*np.exp(-0.5*(k*sigmap)**2)
+    pk = np.exp(-(k*sigmap)**2)
+    if nc % 2 == 0:
+        pk[nc // 2] = 0.0
+
+    pksum = np.sum(pk)
+    sigma_u_raw = math.sqrt(2.0/boxsize*pksum)
+
+    mag = (sigma_u/sigma_u_raw)*np.sqrt(0.5*boxsize*pk)
     
-    uk = amp*(np.random.normal(0.0, amp, nk)
-              + 1j*np.random.normal(0.0, amp, nk))
-
+    uk = mag*(np.random.normal(0.0, 1.0, nk)
+                    + 1j*np.random.normal(0.0, 1.0, nk))    
     uk[0]= 0.0;
-    uk[nc // 2] = 0.0
 
-    ux = np.fft.irfft(uk)/boxsize
-    print('sigma_u', np.std(ux))
+    ux = np.fft.irfft(uk)*(nc/boxsize)
+
+    print('# sigma_u x space = ', np.std(ux), sigma_u)
 
     return ux

logos/power_spectrum.py
@@ -21,12 +21,15 @@ def compute_power_spectrum(grid, *, subtract_shotnoise=True, correct_mas=True,
     grid.interlace()
     
     # window function correction
-    nc = grid.nc
-    nck = nc // 2 + 1
-    i = np.arange(nck)
-    w = np.sin(math.pi/nc*i)
-    w[0] = 1.0
-    w[1:nck] /= math.pi/nc*i[1:nck]
+    if correct_mas:
+        nc = grid.nc
+        nck = nc // 2 + 1
+        i = np.arange(nck)
+        w = np.sin(math.pi/nc*i)
+        w[0] = 1.0
+        w[1:nck] /= math.pi/nc*i[1:nck]
+    else:
+        w = np.ones(nck)
 
     if subtract_shotnoise:
         shotnoise = grid.shot_noise