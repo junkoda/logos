Fix normalisation of 1D power: 1/(2pi)^2.

logos/init.py
@@ -1,16 +1,26 @@
 import math
 import numpy as np
 import logos.cosmology
+from scipy.interpolate import interp1d
 
 def compute_projected_velocity_power(power, *, a=None):
     """Compute 1D power spectrum P_1D(k) from 3D power spectrum P(k)
     Args:
         power (2D array): k P
+        a: normalise the power spectrum by (f*D(a))^2
 
     Returns:
         P1 (array): 1D velocity power spectrum
-        P1(k) = \int dky kz (kx/ky)
+        P1(k) = \int dky kz/(2pi)^2 (kx/k)^2 P(k)/k^2
+        where k = sqrt(kx^2 + ky^2 + kz^2)
     """
+
+    fac = 1.0/(4.0*math.pi)
+    if a is not None:
+        # Normalise by (D(a)*f)^2
+        D = logos.cosmology.D_growth_factor(a)
+        f = logos.cosmology.f_growth_rate(a)
+        fac *= (f*D)**2
 
     k = power[:, 0]
     P = power[:, 1]
@@ -26,19 +36,18 @@ def compute_projected_velocity_power(power, *, a=None):
     
     for i in range(n):
         # trapezoidal integral
-        P1[i] = math.pi*k2[i]*np.sum(f[i:]*dk2[i:])
+        P1[i] = k2[i]*np.sum(f[i:]*dk2[i:])
     
-    return P1
+    return fac*P1
 
-def velocity_field(Pvel, nc, boxsize, *, sigma_u=10.0, sigmap=1.0):
+def velocity_field(Pvel, nc, boxsize, *, sigma_u=None, kmin=None):
     """Generate a velocity field with power spectrum
 
     Args:
-        Pvel np.array -> np.array: velocity power spectrum (function)
+        Pvel: velocity power spectrum (function k -> Pvel)
+              or np.array of k Pvel
         sigma_u (float): normalize Pvel(k) to this velocity rms
-
-    Pvel(k) = A*exp[ -(k*sigmap)**2 ]
-    <u(x)^2> = sigma_u
+                         <u(x)^2> = sigma_u
 
     Returns u(x) array, A
     """
@@ -48,21 +57,41 @@ def velocity_field(Pvel, nc, boxsize, *, sigma_u=10.0, sigmap=1.0):
     dk = 2.0*math.pi/boxsize
 
     k = dk*np.arange(nk)
+    pk = np.zeros(nk)
 
-    pk = Pvel(k) #np.exp(-(k*sigmap)**2)
+    if callable(Pvel):
+        f = Pvel
+    else:
+        # If Pvel is a 2D array of k Pvel, interpolate
+        kmin = Pvel[0, 0]
+        f = interp1d(Pvel[:, 0], Pvel[:, 1])
+    
+
+    if kmin is not None:
+        ikmin = math.ceil(kmin/dk)
+    else:
+        ikmin = 1
+
+    pk[ikmin:] = f(k[ikmin:])
     
     if nc % 2 == 0:
         pk[nc // 2] = 0.0
 
     pksum = np.sum(pk)
-    sigma_u_raw = math.sqrt(2.0/boxsize*pksum)
 
-    mag = (sigma_u/sigma_u_raw)*np.sqrt(0.5*boxsize*pk)
-    
+    if sigma_u:
+        sigma_u_raw = math.sqrt(2.0/boxsize*pksum)
+        rescale = (sigma_u/sigma_u_raw)
+    else:
+        rescale=1.0
+
+    mag = rescale*np.sqrt(0.5*boxsize*pk)
+            
+        
     uk = mag*(np.random.normal(0.0, 1.0, nk)
                     + 1j*np.random.normal(0.0, 1.0, nk))    
     uk[0]= 0.0;
 
     ux = np.fft.irfft(uk)*(nc/boxsize)
 
-    return ux, (sigma_u/sigma_u_raw)**2
+    return ux, rescale**2